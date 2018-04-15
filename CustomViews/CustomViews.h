1335c6060505924f0adaf1a340c8bc11ce5b66ec
Sorin Dolha <SDolha@Hotmail.com>
2018-04-15 16:19:55 +0300

    Initial Commit

---
+++ CustomViews/CustomViews.h
@@ -0,0 +1,19 @@
+//
+//  CustomViews.h
+//  CustomViews
+//
+//  Created by Sorin Dolha on 15.04.2018.
+//  Copyright © 2018 SDolha. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CustomViews.
+FOUNDATION_EXPORT double CustomViewsVersionNumber;
+
+//! Project version string for CustomViews.
+FOUNDATION_EXPORT const unsigned char CustomViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomViews/PublicHeader.h>
+
+
